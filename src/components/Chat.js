a977f9a52e1a3c78a5ce5527568ff66608a84555
Sofiya Tepikin <sofiya@noa.one>
2018-04-15 20:24:00 +0200

    Adding the functionality for POSTing messages and polling new ones

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,37 +1,102 @@
 import React, { Component } from 'react';
-import MessagesFeed from './MessagesFeed'
-import OneFieldInput from './OneFieldInput'
+import queryString from 'query-string';
+import last from 'lodash/last';
+import MessagesFeed from './MessagesFeed';
+import OneFieldInput from './OneFieldInput';
+
+const CHATTY_API = 'https://chatty.kubernetes.doodle-test.com/api/chatty/v1.0/';
+const CHATTY_TOKEN = 'yXrHuTscEfFz';
+const POLLING_INTERVAL = 1000 * 10;
 
 class Chat extends Component {
   constructor() {
     super();
 
     this.state = {
-      messages: []
+      messages: [],
+      latestTimestamp: null,
     };
 
     this.onMessageSubmit = this.onMessageSubmit.bind(this);
+    this.formatMessage = this.formatMessage.bind(this);
+    this.getMessages = this.getMessages.bind(this);
   }
 
   onMessageSubmit(msg) {
-    console.log('///// post this msg: ', msg);
+    this.postMessage(msg)
+      .then(response => response.json())
+      .then(data => {
+          let {messages} = this.state;
+          const latestMessage = this.formatMessage(data);
+          messages.push(latestMessage);
+          this.setState({
+            messages,
+            latestTimestamp: latestMessage.timestamp
+          })
+      }
+    )
   }
 
-  componentWillMount() {
+  postMessage(message) {
+    const data = {
+      message,
+      'author': this.props.username
+    };
+    return fetch(CHATTY_API, {
+        body: JSON.stringify(data),
+        headers: {
+          'token': CHATTY_TOKEN,
+          'content-type': 'application/json'
+        },
+        method: 'POST'
+      })
+  }
 
-    fetch('https://chatty.kubernetes.doodle-test.com/api/chatty/v1.0/?token=yXrHuTscEfFz')
+  formatMessage({message, author, timestamp}) {
+    return {
+      message,
+      author,
+      timestamp: parseInt(timestamp, 10),
+      isCurrentAuthor: author === this.props.username
+    }
+  }
+
+  getMessages() {
+
+    const {latestTimestamp} = this.state;
+
+    let params = {
+      token: CHATTY_TOKEN
+    }
+
+    if (latestTimestamp) {
+      params.since = latestTimestamp;
+    }
+
+    const query = queryString.stringify(params);
+
+    fetch(`${CHATTY_API}?${query}`)
     .then(response => response.json())
     .then(data => {
-      const messages = data.map(({message, author, timestamp}) => {
-        return {
-          message,
-          author,
-          timestamp,
-          isCurrentAuthor: author === this.state.user
-        };
+      if (!data.length) return false;
+      const newMessages = data.map(this.formatMessage);
+      const {messages} = this.state;
+      const latestTimestamp = last(newMessages).timestamp;
+    	this.setState({
+        messages: messages.concat(newMessages),
+        latestTimestamp
       });
-    	this.setState({messages});
     });
+  }
+
+
+  componentWillMount() {
+    this.getMessages();
+    this.pollingInterval = setInterval(this.getMessages, POLLING_INTERVAL);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.pollingInterval);
   }
 
   render() {
